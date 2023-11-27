09ecd189c0ef29ea79486112c8d77b51526cd67f
minhthuan196 <minhthuandtu@gmail.com>
2023-11-27 20:08:33 +0700

    update ss8

--- ss8_validate/bai_tap/register_form/register_form/src/main/java/com/example/register_form/dto/UserDto.java
+++ ss8_validate/bai_tap/register_form/register_form/src/main/java/com/example/register_form/dto/UserDto.java
@@ -34,17 +34,21 @@ public class UserDto implements Validator {
             errors.rejectValue("firstName",null,"First Name have more 3 symbol");
         }else if(!userDto.firstName.trim().matches("^[A-Z][a-z]*$")){
             errors.rejectValue("firstName",null,"First Name not format");
-        }else if("".equals(userDto.lastName)){
+        }
+        if("".equals(userDto.lastName)){
             errors.rejectValue("lastName",null,"Not empty");
         }else if(userDto.lastName.length()<3 || userDto.lastName.length()>=45){
             errors.rejectValue("lastName",null,"Last Name have more 3 symbol");
         }else if(!userDto.lastName.trim().matches("^[A-Z][a-z]+(\\s[A-Z][a-z]+)*$")){
             errors.rejectValue("lastName",null,"Last Name not format");
-        } else if (!userDto.phoneNumber.trim().matches("^0\\d{9,10}$")) {
+        }
+        if (!userDto.phoneNumber.trim().matches("^0\\d{9,10}$")) {
             errors.rejectValue("phoneNumber",null,"Phone number not format");
-        } else if(userDto.age<18 || userDto.age>100){
+        }
+        if(userDto.age<18 || userDto.age>100){
             errors.rejectValue("age",null,"age >=18 and <=100");
-        } else if(!userDto.email.trim().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
+        }
+        if(!userDto.email.trim().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
             errors.rejectValue("email",null,"email not format");
         }
     }
